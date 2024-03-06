097ceef92691dd0053e207b6c83a04d0d2447b89
Nimrajarral <162471218+Nimrajarral@users.noreply.github.com>
2024-03-06 23:55:03 +0500

    Add files via upload

---
+++ comment.js
@@ -0,0 +1,8 @@
+//  Question No 10  //
+// choose two of the programs you comment to each .If you dont have anything specific to write because your program are too simple at this point ,just
+// add your name and the current data  at the top of each programb file .Then write one sentence describing what the program does .
+// exercise 10 
+//My name is Nimra Asif 
+// this is simple program .
+console.log("simple program ");
+console.log(5 * 3);
