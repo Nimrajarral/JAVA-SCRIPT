577ffcb773e5897a0a639a78a4d50be166b59fa1
Nimrajarral <162471218+Nimrajarral@users.noreply.github.com>
2024-03-06 23:51:49 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var quote = "A person who had never mistake never tried anything new";
-var famous_persoon = "Albert Einstien";
-var message = "".concat(famous_persoon, " once said ").concat(quote);
-console.log(message);
+var personName = "\n \t NIMRA ASIF\n\t";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
