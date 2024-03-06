277ada3d381f2d12a947cd29aee30cf60f0d2e4f
Nimrajarral <162471218+Nimrajarral@users.noreply.github.com>
2024-03-06 23:54:15 +0500

    Add files via upload

---
+++ favN.js
@@ -0,0 +1,4 @@
+// store your fav num in a variable.Then using a variable,create a msg that reveals your fav num . Print that message .//
+var fav_num = 4;
+var message = ("my fav num is ".concat(fav_num, "."));
+console.log(message);
