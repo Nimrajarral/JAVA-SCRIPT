bd62eb453d6f8bb4986c51019498430cf2e6fd55
Nimrajarral <162471218+Nimrajarral@users.noreply.github.com>
2024-03-06 23:49:51 +0500

    Add files via upload

---
+++ Quote.js
@@ -0,0 +1,3 @@
+var f_quote = "A person who never made a mistake never tried anything new";
+var author_name = "Albert Einstien";
+console.log(" ".concat(author_name, " once said, ").concat(f_quote));
